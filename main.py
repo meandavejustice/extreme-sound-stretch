12361823b5ac5b7899d37fd061fe184e388ab34a
David Adam Justice <david@outthinkgroup.com>
2014-03-21 17:32:16 -0700

    lets see if pep8 will work

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from werkzeug import secure_filename, SharedDataMiddleware
 
 DOWNLOAD_FOLDER = 'downloads/'
 UPLOAD_FOLDER = 'uploads/'
-ALLOWED_EXTENSIONS = set(['mp3', 'mp4', 'wav', 'ogg', 'flac', 'gif'])
+ALLOWED_EXTENSIONS = set(['mp3', 'mp4', 'wav', 'ogg', 'flac', 'gif', 'try', 'to', 'break', 'char', 'limit'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
